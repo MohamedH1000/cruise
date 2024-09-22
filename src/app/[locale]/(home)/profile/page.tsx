6531c13c0a43f166e3bc62bb4bc18b42b48e5416
MohamedH1000 <mohammedhisham115@gmail.com>
2024-09-23 00:59:11 +0300

    protect the profile route for logged in users only

--- src/app/[locale]/(home)/profile/page.tsx
+++ src/app/[locale]/(home)/profile/page.tsx
@@ -1,6 +1,14 @@
+import { redirect } from "@/i18n/routing";
+import { getCurrentUser } from "@/lib/actions/user.action";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    redirect("/sign-in");
+  }
+
   return <div className="mt-[120px]">My Profile</div>;
 };
 
