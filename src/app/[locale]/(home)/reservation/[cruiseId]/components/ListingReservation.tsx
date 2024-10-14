5074ef3e9996a75de8bb24755fdf7030660e3671
MohamedH1000 <mohammedhisham115@gmail.com>
2024-10-14 17:10:08 +0300

    adjust the reservation system

--- src/app/[locale]/(home)/reservation/[cruiseId]/components/ListingReservation.tsx
+++ src/app/[locale]/(home)/reservation/[cruiseId]/components/ListingReservation.tsx
@@ -5,36 +5,57 @@ import React, { useEffect, useMemo, useState } from "react";
 import Calender from "./Calender";
 import { Separator } from "@/components/ui/separator";
 import { motion } from "framer-motion";
-import PhoneInput from "react-phone-number-input";
 import "react-phone-number-input/style.css";
-// import {
-//   AlertDialog,
-//   AlertDialogAction,
-//   AlertDialogCancel,
-//   AlertDialogContent,
-//   AlertDialogDescription,
-//   AlertDialogFooter,
-//   AlertDialogHeader,
-//   AlertDialogTitle,
-//   AlertDialogTrigger,
-// } from "@/components/ui/alert-dialog";
-import { Input } from "@/components/ui/input";
+
 import { differenceInCalendarDays, eachDayOfInterval } from "date-fns";
 import { useTranslations } from "next-intl";
+import { Button } from "@/components/ui/button";
+import DetailsForm from "./DetailsForm";
 const initialDateRange = {
   startDate: new Date(),
   endDate: new Date(),
   key: "selection",
 };
+
+enum STEPS {
+  DATA = 0,
+  ATTRACTIONS = 1,
+  RESTAURANTS = 2,
+}
 const ListingReservation = ({ reservations, currentUser, cruise }: any) => {
   const t = useTranslations();
   const [name, setName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [dateRange, setDateRange] = useState<Range>(initialDateRange);
   const [totalPrice, setTotalPrice] = useState<any>();
   const [numberOfAttractions, setNumberOfAttractions] = useState<any>();
-  console.log(numberOfAttractions);
+  const [steps, setSteps] = useState(STEPS.DATA);
+
+  const onBack = () => {
+    setSteps((value) => value - 1);
+  };
+
+  const onNext = () => {
+    setSteps((value) => value + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (steps === STEPS.RESTAURANTS) {
+      return t("translations.reservenow");
+    }
+    return t("translations.next");
+  }, [steps]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (steps === STEPS.DATA) {
+      return undefined;
+    }
+
+    return t("translations.previous");
+  }, [steps]);
+  // console.log(numberOfAttractions);
 
   useEffect(() => {
     if (dateRange.startDate && dateRange.endDate) {
@@ -67,6 +88,8 @@ const ListingReservation = ({ reservations, currentUser, cruise }: any) => {
       });
     return dates;
   }, [reservations]);
+
+  const onSubmit = async () => {};
   return (
     <motion.div
       className="bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden mt-5 w-full"
@@ -76,159 +99,52 @@ const ListingReservation = ({ reservations, currentUser, cruise }: any) => {
       viewport={{ once: true }}
     >
       <div className="flex flex-row items-center gap-1 p-4">
-        <div className="text-2xl font-semibold">SAR {cruise.price}</div>
+        <div className="text-2xl font-semibold">SAR {cruise?.price}</div>
         <div className="font-light text-neutral-600">
           {t("translations.pernight")}
         </div>
       </div>
       <Separator />
-      <Calender
-        value={dateRange}
-        onChange={(value) => {
-          setDateRange(value.selection);
-        }}
-        disabledDates={disableDates}
-      />
-      {!currentUser ? (
-        <>
-          <Separator />
-          <div className="flex flex-col px-4 py-4 gap-3">
-            <h1 className="text-[30px] font-medium max-md:text-[20px]">
-              {t("translations.userDetails")}
-            </h1>
-            <label htmlFor="name">{t("SignUp.name")}</label>
-            <Input
-              name="name"
-              onChange={(e) => setName(e.target.value)}
-              placeholder={t("translations.fullName")}
-              required
-            />
-            <label htmlFor="email">{t("Login.email")}</label>
-            <Input
-              name="email"
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder={t("Login.email")}
-              required
-            />
-            <label htmlFor="phonenumber">{t("SignUp.phonenumber")}</label>
-            <div dir="ltr">
-              <PhoneInput
-                name="phoneNumber"
-                defaultCountry="US"
-                international
-                withCountryCallingCode
-                onChange={(value: any) => setPhoneNumber(value)}
-                placeholder={t("SignUp.phonenumber")}
-                required
-                className="border-[1px] border-[#bda069] p-3 rounded-md focus:outline-none !important"
-              />
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="px-4 mb-5">
-          <label htmlFor="phonenumber">
-            {t("SignUp.phonenumber")} ({t("translations.mandatory")})
-          </label>
-          <div dir="ltr" className="mt-2">
-            <PhoneInput
-              name="phoneNumber"
-              defaultCountry="US"
-              international
-              withCountryCallingCode
-              onChange={(value: any) => setPhoneNumber(value)}
-              placeholder={t("SignUp.phonenumber")}
-              required
-              className="border-[1px] border-[#bda069] p-3 rounded-md focus:outline-none !important"
-            />
-          </div>
-        </div>
+      {steps === STEPS.DATA && (
+        <DetailsForm
+          name={name}
+          setName={setName}
+          email={email}
+          setEmail={setEmail}
+          phoneNumber={phoneNumber}
+          setPhoneNumber={setPhoneNumber}
+          dateRange={dateRange}
+          setDateRange={setDateRange}
+          disableDates={disableDates}
+          currentUser={currentUser}
+        />
       )}
 
       <Separator />
-      <div className="p-4">
-        {/* <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <Button
-              disabled={isLoading ? true : false}
-              label="Reserve"
-              className="w-full rounded-md bg-[#bda069]
-        text-white border-[#bda069] hover:border-[1px] hover:bg-white 
-        hover:text-[#bda069] transtion duration-300 font-bold"
-            >
-              {isLoading ? "برجاء الانتظار" : "قم بالحجز"}
-            </Button>
-          </AlertDialogTrigger>
-          <AlertDialogContent className="flex flex-col items-start">
-            <AlertDialogHeader>
-              <AlertDialogTitle className="text-start">
-                بيانات الحجز
-              </AlertDialogTitle>
-              <AlertDialogDescription>
-                <div className="flex justify-between items-center gap-5">
-                  <div className="text-right">
-                    <h1>من تاريخ:</h1>
-                    <p>{dateRange?.startDate?.toString()}</p>
-                  </div>
-                  <div className="text-right">
-                    <h1>الى تاريخ:</h1>
-                    <p>{dateRange?.endDate?.toString()}</p>
-                  </div>
-                </div>
-                <Separator className="mt-5" />
-                <div className="flex flex-col items-start w-full">
-                  <div className="flex justify-between items-center w-full mt-2">
-                    {servicePrice.chairPrice > 0 && (
-                      <>
-                        <h1>كراسي حفلات</h1>
-                        <p>{servicePrice.chairPrice * 200} SAR</p>
-                      </>
-                    )}
-                  </div>
-                  <div className="flex justify-between items-center w-full mt-2">
-                    {servicePrice.coffeePrice > 0 && (
-                      <>
-                        <h1>ضيافة قهوة</h1>
-                        <p>{servicePrice.coffeePrice * 250} SAR</p>
-                      </>
-                    )}
-                  </div>
-                  <div className="flex justify-between items-center w-full mt-2">
-                    {servicePrice.sweetPrice > 0 && (
-                      <>
-                        <h1>ضيافة حلى</h1>
-                        <p>{servicePrice.sweetPrice * 150} SAR</p>
-                      </>
-                    )}
-                  </div>
-                  <div className="flex justify-between items-center w-full mt-2">
-                    {servicePrice.tablePrice > 0 && (
-                      <>
-                        <h1>سفرة طعام</h1>
-                        <p>{servicePrice.tablePrice * 100} SAR</p>
-                      </>
-                    )}
-                  </div>
-                  {servicePrice.chairPrice === 0 &&
-                    servicePrice.sweetPrice === 0 &&
-                    servicePrice.tablePrice === 0 &&
-                    servicePrice.coffeePrice === 0 && (
-                      <div className="mb-5">لم يتم اضافة خدمات اضافية</div>
-                    )}
-                  <Separator />
-                  <div className="mt-5 flex justify-between items-center w-full">
-                    <h1>المجموع</h1>
-                    <p>{totalPrice} SAR</p>
-                  </div>
-                </div>
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter className="gap-5 max-md:flex-col max-md:gap-2 rounded-md">
-              <AlertDialogAction onClick={onSubmit}>تاكيد</AlertDialogAction>
-              <AlertDialogCancel>الغاء</AlertDialogCancel>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog> */}
+      <div className="p-4 flex justify-center items-center gap-2">
+        {secondaryActionLabel && (
+          <Button
+            onClick={onBack}
+            disabled={isLoading ? true : false}
+            className="w-full rounded-md bg-[#003b95]
+          text-white border-[#003b95] hover:border-[1px] hover:bg-white 
+          hover:text-[#003b95] transtion duration-300 font-bold"
+          >
+            {secondaryActionLabel}
+            {/* {isLoading ? "برجاء الانتظار" : "قم بالحجز"} */}
+          </Button>
+        )}
+
+        <Button
+          onClick={actionLabel === t("translations.next") ? onNext : onSubmit}
+          disabled={isLoading ? true : false}
+          className="w-full rounded-md bg-[#003b95]
+        text-white border-[#003b95] hover:border-[1px] hover:bg-white 
+        hover:text-[#003b95] transtion duration-300 font-bold"
+        >
+          {actionLabel}
+          {/* {isLoading ? "برجاء الانتظار" : "قم بالحجز"} */}
+        </Button>
       </div>
       <div className="p-4 flex flex-row  items-center justify-between font-semibold text-lg">
         <div className="font-bold capitalize">{t("translations.total")}:</div>
