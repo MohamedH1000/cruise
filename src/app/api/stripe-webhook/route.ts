6d7445f82f0164b48eacb0f2cec5ce371d9e42b1
MohamedH1000 <mohammedhisham115@gmail.com>
2024-10-26 18:46:41 +0300

    test the success page in production

--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 import { buffer } from "micro";
 import prisma from "@/lib/prisma";
 import { stripe } from "@/lib/stripe";
@@ -7,42 +7,42 @@ export const config = {
   api: { bodyParser: false },
 };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export async function POST(req: NextRequest) {
+  console.log("stripe webhook received");
   const buf = await buffer(req);
-  const signature = req.headers["stripe-signature"] as string;
+  const signature = req.headers.get("stripe-signature");
 
   try {
     const event = stripe.webhooks.constructEvent(
       buf,
-      signature,
+      signature!,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
 
     if (event.type === "checkout.session.completed") {
       const session = event.data.object;
-      const reservId = session?.metadata.reservationId;
+      const reservId = session?.metadata?.reservationId;
 
-      // Update the reservation status to 'active' in the database
       await prisma.reservation.update({
         where: { id: reservId },
         data: { status: "active" },
       });
     } else if (event.type === "checkout.session.expired") {
       const session = event.data.object;
-      const reservId = session?.metadata.reservationId;
+      const reservId = session?.metadata?.reservationId;
 
-      // Update the reservation status to 'failed' in the database
       await prisma.reservation.update({
         where: { id: reservId },
         data: { status: "failed" },
       });
     }
 
-    res.status(200).json({ received: true });
-  } catch (err) {
-    res.status(400).send(`Webhook Error: ${err.message}`);
+    return NextResponse.json({ received: true });
+  } catch (err: any) {
+    console.error("Webhook Error:", err.message);
+    return NextResponse.json(
+      { error: `Webhook Error: ${err.message}` },
+      { status: 400 }
+    );
   }
 }
