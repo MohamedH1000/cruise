9f976ec65af3d103f967fbfd841b7c4bb8853257
MohamedH1000 <mohammedhisham115@gmail.com>
2024-10-27 20:49:36 +0300

    adjust the stripe webhook

--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -2,18 +2,22 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { stripe } from "@/lib/stripe";
 
+export const config = {
+  api: { bodyParser: false }, // Only needed in /pages/api
+};
+
 export async function POST(req: NextRequest) {
   console.log("stripe webhook received");
 
   // Get the raw body
-  const body = await req.text();
-
+  const body = await req.arrayBuffer(); // Fetch the raw body as ArrayBuffer
+  const buf = Buffer.from(body); // Convert ArrayBuffer to Buffer
   const sig = req.headers.get("stripe-signature");
 
   try {
     // Attempt to construct the event from the raw body and signature
     const event = stripe.webhooks.constructEvent(
-      body,
+      buf,
       sig!,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
